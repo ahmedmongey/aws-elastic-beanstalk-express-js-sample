2fe7989a8ea3ecffd187e86d25cab8bf364ba67f
ahmedmongey <112070317+ahmedmongey@users.noreply.github.com>
2022-11-28 12:27:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,22 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-const fs = require('fs') // this engine requires the fs module
-app.engine('ntl', (filePath, options, callback) => { // define the template engine
-  fs.readFile(filePath, (err, content) => {
-    if (err) return callback(err)
-    // this is an extremely simple template engine
-    const rendered = content.toString()
-      .replace('#title#', `<title>${options.title}</title>`)
-      .replace('#message#', `<h1>${options.message}</h1>`)
-    return callback(null, rendered)
-  })
-})
-app.set('view engine', 'ntl') // register the template engine
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
-})
-
+app.get('/', (req, res) => res.send('Hello from DevOps World! I Love You '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
